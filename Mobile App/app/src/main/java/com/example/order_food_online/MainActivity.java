ea7fcb447408f75375de83ad5c12cfc375770cb9
Bendy-Robull <72128924+Bendy-Robull@users.noreply.github.com>
2021-03-19 21:32:25 +0100

    Add files via upload

---
+++ Mobile App/app/src/main/java/com/example/order_food_online/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.order_food_online;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
